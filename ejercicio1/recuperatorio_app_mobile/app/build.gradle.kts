7a298d4c703d6f12b17bd19d14096e04dfa1b31a
Cris8049 <cristopher070404@gmail.com>
2025-11-26 16:37:38 -0400

    feat: Add dependencies

--- ejercicio1/recuperatorio_app_mobile/app/build.gradle.kts
+++ ejercicio1/recuperatorio_app_mobile/app/build.gradle.kts
@@ -56,4 +56,5 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    implementation("com.google.accompanist:accompanist-permissions:0.37.3")
 }
